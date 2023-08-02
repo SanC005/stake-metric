73dba41577c6da3e312ed74266c46d9a93240126
SanC005 <sandeepcsrao@gmail.com>
2023-08-02 19:27:28 +0530

    completed mapping data fetched to the webpage

--- src/app/components/details.jsx
+++ src/app/components/details.jsx
@@ -1,40 +1,103 @@
-import React from 'react'
-
-async function getData(coin) {
-    const res = await fetch(`https://${coin}.api.subscan.io/api/scan/metadata`,{
-      method:"POST",
-      headers: {
-        "Content-Type": "application/json",
-        "x-api-key":"71ddb4a6fb274c30b95b19df6af89ed9"
-      },
-      body:JSON.stringify({
-  
-      })
-    })
-    // .then((res) => res.json()).catch((e) => console.log(e))
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
+import React, { useEffect, useState } from "react";
+function fetchData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "x-api-key": `${process.env.API_KEY}`,
+    },
+    body: JSON.stringify({}),
+  }).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+    //   throw new Error("Network response was not ok.");
+        console.log("error")
     }
-    return res.json()
+  })
+  .then((data) => {
+    resolve(data);
+  })
+  .catch((error) => {
+    reject(error);
+  });
+  });
 }
-export default async function Details({name,index}) {
-    const geted = await getData(name);
-    const newData = geted.data;
-  return (
-    <div className='flex h-full'>
-            <div className='w-1/2 p-10 font-mono'>
-              <div className='h-1/3 text-lg font-semibold'>{name}</div>
-              <div className='grid grid-cols-2 gap-10 font-thin '>
-                <div><div>Finalized</div><div>{newData.finalized_blockNum}</div></div>
-                <div><div>Signed Extrinics</div><div>{newData.count_signed_extrinsic}</div></div>
-                <div><div>Transfers</div><div>{newData.count_transfer}</div></div>
-                <div><div>Current Pool</div><div>{newData.currentPools}</div></div>
+//     fetch(url)
+//       .then((response) => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error("Network response was not ok.");
+//         }
+//       })
+
+//       .then((data) => {
+//         resolve(data);
+//       })
+
+//       .catch((error) => {
+//         reject(error);
+//       });
+//   });
+// }
+
+// async function getData(coin) {
+//   const res = await fetch(`https://${coin}.api.subscan.io/api/scan/metadata`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "x-api-key": "71ddb4a6fb274c30b95b19df6af89ed9",
+//     },
+//     body: JSON.stringify({}),
+//   });
+//   // .then((res) => res.json()).catch((e) => console.log(e))
+//   if (!res.ok) {
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
+export default function Details({index}) {
+  const [val,setVal] = useState({});
+  const x = {
+    0:"cardona",
+    1:"polkadot",
+    2:"kusama"
+  }
+  fetchData(`https://${x[index]}.api.subscan.io/api/scan/metadata`).then(function(result){
+      console.log("setting")
+          console.log(result)
+          setVal(result)
+          console.log("setted")
+        })
   
-              </div>
-            </div>
-            <div className='w-1/2 p-10'>
-              {/* <div className='text-lg font-semibold'>Graph</div> */}
-            </div>
+  return (
+    <div className="flex h-full">
+      <div className="w-1/2 p-10 font-mono">
+        <div className="h-1/3 text-lg font-semibold">{x[index]}</div>
+        <div className="grid grid-cols-2 gap-10 font-thin ">
+          <div>
+            <div onClick={() => alert()}>Finalized</div>
+            <div>{val?.data?.finalized_blockNum}</div>
           </div>
-  )
+          <div>
+            <div>Signed Extrinics</div>
+            <div>{val?.data?.count_signed_extrinsic}</div>
+          </div>
+          <div>
+            <div>Transfers</div>
+            <div>{val?.data?.count_transfer}</div>
+          </div>
+          <div>
+            <div>Current Pool</div>
+            <div>{val?.data?.currentPools}</div>
+          </div>
+        </div>
+      </div>
+      <div className="w-1/2 p-10">
+        {/* <div className='text-lg font-semibold'>Graph</div> */}
+      </div>
+    </div>
+  );
 }
--- src/app/components/metric.jsx
+++ src/app/components/metric.jsx
@@ -3,17 +3,27 @@ import {React,useState} from 'react'
 import Details from './details'
 
 function Metric() {
-    var navstyle = 'hover:bg-pink-300 hover:text-white w-full flex items-center justify-center'
-    const [index,setIndex] = useState(0);
+    var navstyle = 'hover:bg-pink-300 w-full flex items-center justify-center hover:cursor-pointer'
+    const [index,setIndex] = useState(1);
     const name ="polkadot"
+    function changeIndex(e){
+        if(e=="polkadot"){
+            setIndex(1);
+        }else if(e=="cardona"){
+            setIndex(0);
+        }
+        else if(e=="kusama"){
+            setIndex(2);
+        }
+    }
   return (
     <div className='bg-gradient-to-r from-blue-100 via-indigo-200 to-pink-400 h-1/2 text-black w-5/6 rounded-2xl m-5'>
           <div className='flex justify-evenly bg-gradient-to-r from-blue-100 via-slate-200 to-pink-200 text-center rounded-t-2xl h-1/5 text-lg'>
-            <div className={`${navstyle} rounded-tl-2xl`} onClick={() => setIndex(0)}>Cardona</div>
-            <div className={`${navstyle}` } onClick={() => setIndex(1)}  >PolkaDot</div>
-            <div className={`${navstyle} rounded-tr-2xl`} onClick={() => setIndex(2)}>Kusama</div>
+            <div className={`${navstyle} ${index==0?"bg-pink-400":""} rounded-tl-2xl`} onClick={() => {setIndex(0)}}>Cardona</div>
+            <div className={`${navstyle} ${index==1?"bg-pink-400":""}` } onClick={() => changeIndex("polkadot")} ><button>PolkaDot</button></div>
+            <div className={`${navstyle} ${index==2?"bg-pink-400":""} rounded-tr-2xl`} onClick={() => {setIndex(2)}}>Kusama</div>
           </div>
-          <Details name={name} index={index}/>
+          <Details index={index}/>
         </div>
   )
 }
